f5d9119eb2adc82f58d2ebec42269b2801b88324
eduardomantz291 <eduardomantz2112@gmail.com>
2020-09-11 23:39:58 -0300

    erro corregido

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-if (process.env.NODE.ENV == "production") {
+if (process.env.NODE_ENV == "production") {
   module.exports = {mongoURL: "mongodb+srv://eduardoblogapp:eduardoblogapp@cluster0.oxsnb.mongodb.net/blogapp?retryWrites=true&w=majority"}
 }else {
   module.exports = {mongoURL: "mongodb://localhost/blogapp"}
